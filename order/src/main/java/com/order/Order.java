73f243e5adf4bf02ff89a5212092e6709d3dd286
Gimi818 <gimi8183@gmail.com>
2024-03-17 19:43:15 +0100

    Created Order entity

---
+++ order/src/main/java/com/order/Order.java
@@ -0,0 +1,26 @@
+package com.order;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private OrderStatus orderStatus;
+    private BigDecimal totalPrice;
+
+
+}
