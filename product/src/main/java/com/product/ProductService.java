778bf8436be6d5d7c3db1d3085ef5c783fefa02f
Gimi818 <gimi8183@gmail.com>
2024-03-18 09:02:36 +0100

    Added findProductsById and findAllProducts methods to ProductService

--- product/src/main/java/com/product/ProductService.java
+++ product/src/main/java/com/product/ProductService.java
@@ -2,9 +2,13 @@ package com.product;
 
 import com.product.dto.NewProductDto;
 import com.product.dto.ProductResponseDto;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -13,14 +17,24 @@ public class ProductService {
     private final ProductRepository repository;
     private final ProductMapper mapper;
 
-
+    @Transactional
     public ProductResponseDto addProduct(NewProductDto newProductDto) {
         Product product = repository.save(mapper.dtoToEntity(newProductDto));
         log.info("Added product {}, stock quantity = {}", newProductDto.name(), newProductDto.StockQuantity());
         return mapper.productToResponseDto(product);
     }
 
+    public ProductResponseDto findProductById(Long id) {
+        Product product = repository.findById(id).orElseThrow();
+        log.info("Found product with ID {}", id);
+        return mapper.productToResponseDto(product);
+    }
 
+    public List<ProductResponseDto> findAllProducts() {
+        log.info("Find all products");
+        return repository.findAll().stream().map(mapper::productToResponseDto)
+                .collect(Collectors.toList());
+    }
 
 
 }
