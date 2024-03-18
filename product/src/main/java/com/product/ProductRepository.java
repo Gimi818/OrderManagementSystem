758af547f768aeb17ff88d4c29526813151a9257
Gimi818 <gimi8183@gmail.com>
2024-03-18 10:27:39 +0100

    Implement queries for product sorting and filtering

--- product/src/main/java/com/product/ProductRepository.java
+++ product/src/main/java/com/product/ProductRepository.java
@@ -1,6 +1,24 @@
 package com.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE p.productCategory =:productCategory")
+    List<Product> findProductByProductCategory(@Param("productCategory") ProductCategory productCategory);
+
+    @Query("SELECT p FROM Product p WHERE p.price >= :minPrice AND p.price <= :maxPrice")
+    List<Product> findProductsByPriceRange(@Param("minPrice") BigDecimal minPrice, @Param("maxPrice") BigDecimal maxPrice);
+
+    @Query("SELECT p FROM Product p ORDER BY p.price ASC")
+    List<Product> findAllProductsSortedByPriceAsc();
+
+    @Query("SELECT p FROM Product p ORDER BY p.price DESC")
+    List<Product> findAllProductsSortedByPriceDesc();
+
 }
