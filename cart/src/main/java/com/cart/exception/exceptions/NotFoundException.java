fe13540183e2006b094ee569bf28a01f414dd0d6
Gimi818 <gimi8183@gmail.com>
2024-03-19 08:15:10 +0100

    Added AlreadyExistException and NotFoundException

---
+++ cart/src/main/java/com/cart/exception/exceptions/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.cart.exception.exceptions;
+
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(final String message, final Object... args) {
+        super(String.format(message, args));
+    }
+
+}
