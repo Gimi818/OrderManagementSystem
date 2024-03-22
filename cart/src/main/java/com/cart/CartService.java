76325577964f96b4817f37b74cc8c18a012467bc
Gimi818 <gimi8183@gmail.com>
2024-03-22 09:53:03 +0100

    Added @Transactional annotation in methods

--- cart/src/main/java/com/cart/CartService.java
+++ cart/src/main/java/com/cart/CartService.java
@@ -5,6 +5,7 @@ import com.cart.dto.CartDto;
 import com.cart.dto.UserDto;
 import com.cart.exception.exceptions.AlreadyExistException;
 import com.cart.exception.exceptions.NotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,7 @@ import java.util.Map;
 public class CartService {
     private final CartRepository repository;
 
+    @Transactional
     public void createUserCart(UserDto user) {
         log.debug("Attempting to create a cart for user ID: {}", user.id());
         verifyCartDoesNotExist(user);
@@ -27,13 +29,7 @@ public class CartService {
         log.info("Cart created for user ID: {}", user.id());
     }
 
-    public void verifyCartDoesNotExist(UserDto user) {
-        if (repository.existsById(user.id())) {
-            log.warn("Attempt to create a cart for user ID: {} failed. Cart already exists.", user.id());
-            throw new AlreadyExistException(CART_IS_EXIST, user.id());
-        }
-    }
-
+    @Transactional
     public void addProductToCart(AddProductDto newProduct) {
         log.debug("Attempting to add product to cart. Cart ID: {}, Product id: {}", newProduct.userId(), newProduct.productId());
         Cart cart = findCartById(newProduct.userId());
@@ -43,6 +39,7 @@ public class CartService {
         log.info("Product added to cart. Cart ID: {}, Product ID: {}, Quantity: {}", newProduct.userId(), newProduct.productId(), newProduct.quantity());
     }
 
+    @Transactional
     public void removeProductFromCart(Long cartId, Long productId) {
         log.debug("Attempting to remove product from cart. Cart ID: {}, Product ID: {}", cartId, productId);
         Cart cart = findCartById(cartId);
@@ -66,16 +63,25 @@ public class CartService {
         log.info("Cart found with ID: {}", cartId);
         return cart;
     }
+
     public CartDto getCartContents(Long cartId) {
         return repository.findById(cartId)
                 .map(cart -> new CartDto(cart.getItems()))
                 .orElseThrow(() -> new NotFoundException(NOT_FOUND_BY_ID, cartId));
     }
+
     public Cart createEmptyCartForUser(UserDto user) {
         Cart cart = new Cart();
         cart.setId(user.id());
         cart.setItems(new HashMap<>());
         return cart;
+    }
+
+    public void verifyCartDoesNotExist(UserDto user) {
+        if (repository.existsById(user.id())) {
+            log.warn("Attempt to create a cart for user ID: {} failed. Cart already exists.", user.id());
+            throw new AlreadyExistException(CART_IS_EXIST, user.id());
+        }
     }
 
     static final class ErrorMessages {
