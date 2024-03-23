5e0b105cd79e2e427c2681f6b6454e389da67346
Gimi818 <gimi8183@gmail.com>
2024-03-23 23:34:08 +0100

    Created OrderConfirmationEmail with  sendEmail basic method

---
+++ emailsender/src/main/java/com/emailsender/OrderConfirmationEmail.java
@@ -0,0 +1,32 @@
+package com.emailsender;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+@Service
+@AllArgsConstructor
+public class OrderConfirmationEmail {
+    private final JavaMailSender javaMailSender;
+
+    private void sendEmail(String recipientEmail) throws MessagingException {
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", Locale.getDefault());
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(recipientEmail);
+        helper.setSubject(bundle.getString("email.subject"));
+        helper.setText(bundle.getString("email.thankYouMessage"), true);
+
+
+        javaMailSender.send(message);
+    }
+
+
+}
