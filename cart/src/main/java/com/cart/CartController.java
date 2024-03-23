ad74c43e90436e752f5c7b43fe56cc8a2bbd1a37
Gimi818 <gimi8183@gmail.com>
2024-03-23 12:52:31 +0100

    Implementation deleteProduct endpoint

--- cart/src/main/java/com/cart/CartController.java
+++ cart/src/main/java/com/cart/CartController.java
@@ -4,6 +4,7 @@ import com.cart.dto.CartDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +14,25 @@ import static com.cart.CartController.Routes.*;
 @RestController
 @RequiredArgsConstructor
 public class CartController {
-    private final CartService service;
+    private final CartService cartService;
+    private final CartRetrievalService cartRetrievalService;
 
     @GetMapping(GET_CART_CONTENTS)
     public ResponseEntity<CartDto> getCartContents(@PathVariable Long cartId) {
-        CartDto cartDto = service.getCartContents(cartId);
+        CartDto cartDto = cartRetrievalService.getCartContents(cartId);
         return new ResponseEntity<>(cartDto, HttpStatus.OK);
     }
 
+    @DeleteMapping(DELETE_PRODUCT_FROM_CART)
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long cartId, @PathVariable Long productId) {
+        cartService.removeProductFromCart(cartId, productId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
     static final class Routes {
-        static final String ROOT = "/api/v1/cart";
+        static final String ROOT = "/api/v1/carts";
         static final String GET_CART_CONTENTS = ROOT + "/{cartId}/contents";
+        static final String DELETE_PRODUCT_FROM_CART = ROOT + "/{cartId}/{productId}";
 
     }
 
