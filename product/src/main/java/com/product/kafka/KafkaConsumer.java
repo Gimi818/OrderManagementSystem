13ba6d30663bfab2ae72bfe610f00951db51c806
Gimi818 <gimi8183@gmail.com>
2024-05-06 21:32:37 +0200

    Created consumeOrder method in KafkaConsumer

--- product/src/main/java/com/product/kafka/KafkaConsumer.java
+++ product/src/main/java/com/product/kafka/KafkaConsumer.java
@@ -2,7 +2,8 @@ package com.product.kafka;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.product.ProductService;
+import com.product.stockManager.StockManager;
+import com.product.dto.order.StockVerificationOrder;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,17 +15,20 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumer {
 
     private final ObjectMapper objectMapper;
-    private final ProductService productService;
+    private final StockManager stockManager;
 
 
-//    @KafkaListener(topics = "order-initiated")
-//    public void onOrderInitiated(String userIdDtoMessage) {
-//
-//        boolean productsAvailable = productService.checkProductsAvailability(orderDetails);
-//
-//    }
-
-
-
+    @KafkaListener(topics = "stockVerification",groupId = "order-group")
+    public void consumeOrder(String message) {
+        log.debug("Received message from Kafka: {}", message);
+        try {
+            StockVerificationOrder stockVerificationOrder = objectMapper.readValue(message, StockVerificationOrder.class);
+            log.info("Deserialized message to stockVerificationOrder with ID: {}", stockVerificationOrder.id());
+            stockManager.checkInventory(stockVerificationOrder);
+            log.info("Successfully created order for stockVerificationOrder ID: {}", stockVerificationOrder.id());
+        } catch (JsonProcessingException e) {
+            log.error("Error deserializing message to stockVerificationOrder: {}", e.getMessage());
+        }
+    }
 
 }
