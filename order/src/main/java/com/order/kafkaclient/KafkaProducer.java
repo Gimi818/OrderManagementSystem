55d876b8776d5eac1c90c2465ff5aa548bd71848
Gimi818 <gimi8183@gmail.com>
2024-06-07 00:35:00 +0200

    Code refactoring in KafkaProducer class

--- order/src/main/java/com/order/kafkaclient/KafkaProducer.java
+++ order/src/main/java/com/order/kafkaclient/KafkaProducer.java
@@ -2,6 +2,8 @@ package com.order.kafkaclient;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.order.Order;
+import com.order.dto.OrderEmail;
 import com.order.dto.StockVerificationOrder;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -21,17 +23,29 @@ public class KafkaProducer {
 
 
     public void verificationOrder(String topic, StockVerificationOrder order) {
+        sendMessage(topic, order);
+    }
 
+    public void sendConfirmationOrderEmail(String topic, OrderEmail order) {
+        sendMessage(topic, order);
+    }
+
+    private void sendMessage(String topic, Object message) {
         try {
-            String orderJson = objectMapper.writeValueAsString(order);
-            CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, orderJson).toCompletableFuture();
-            future.thenAccept(result -> log.info("Successfully sent message to topic: {}, orderId: {}", topic, order.id()))
-                    .exceptionally(ex -> {
-                        log.error("Failed to send message to topic: {}, error: {}", topic, ex.getMessage());
-                        return null;
-                    });
+            String messageJson = objectMapper.writeValueAsString(message);
+            CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, messageJson).toCompletableFuture();
+
+            future.whenComplete((result, ex) -> {
+                if (ex == null) {
+                    log.info("Successfully sent message to topic: {}, message: {}", topic, message);
+                } else {
+                    log.error("Failed to send message to topic: {}, error: {}", topic, ex.getMessage());
+                }
+            });
         } catch (JsonProcessingException e) {
-            log.error("Could not serialize order object to JSON", e);
+            log.error("Could not serialize message object to JSON", e);
         }
     }
+
+
 }
